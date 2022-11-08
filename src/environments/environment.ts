0fc83cfb3a29e0d6d00d1c5100038bfc428f1598
Paulo Roberto da Silva <paulo.silva@avivar.com.br>
2022-11-08 14:58:41 -0300

    segundo commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:8081/api',
+  // api: 'http://localhost:8081/api',
+  api: 'http://siaemailsignature-env-1.eba-ezei4jza.us-east-1.elasticbeanstalk.com/api',
   ws: 'ws://localhost:8081/websocket',
   reports:'http://192.168.0.19:8080/siaaptrelatorio/api'
 };
