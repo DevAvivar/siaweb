114ed8569d9db9eea39b669d0366f98e26302bfb
Desenvolvimento Avivar <119051074+DevAvivar@users.noreply.github.com>
2022-12-23 10:51:20 -0300

    Add files via upload
    alteraçoes no myheader

--- src/app/_shared/myHeader/my-header.component.ts
+++ src/app/_shared/myHeader/my-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { interval, Observable } from 'rxjs';
+import { Unit } from 'src/app/pages/unit/Unit.model';
 import { UserSiaListComponent } from 'src/app/pages/userSia/user-sia-list/user-sia-list.component';
 import { UserSiaService } from 'src/app/pages/userSia/user-sia.service';
 import { UserSia } from 'src/app/pages/userSia/UserSia.model';
@@ -15,18 +16,20 @@ export class MyHeaderComponent implements OnInit {
   loop$!: Observable<any>;
   userSia!: UserSia;
   userLogged!: UserSia;
+  // unitLogged!: Unit;
   logged!: boolean;
   nameUser!: string;
   codeUser!: number;
 
   constructor(
     private router: Router,
-    private userSiaService: UserSiaService, 
+    private userSiaService: UserSiaService
     ) { }
 
   ngOnInit(): void {
     // console.log("LOCALSTORAGE  = ", this.userSiaService.getLocalStorageUser());
     this.userLogged = this.userSiaService.getLocalStorageUser();
+    // this.unitLogged = this.userSiaService.getLocalStorageUnit();
     // this.nameUser = this.userLogged.name;
     // this.codeUser = this.userLogged.code;
   }
